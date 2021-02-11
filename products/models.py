48de138466957ab619abe79ba1959dc44488a2e6
Alena <alynka2991@mail.ru>
2021-02-11 14:47:18 +0300

    Add functions __str__ to models

--- products/models.py
+++ products/models.py
@@ -9,6 +9,9 @@ class Measure(models.Model):
     short_measure_name = models.CharField(max_length=10, null=True, blank=True, unique=True, default=measure_name,
                                           help_text="Короткое наименование единицы измерения")
 
+    def __str__(self):
+        return self.measure_name
+
 
 # Модель Category - Категории продуктов
 class Category(models.Model):
@@ -17,15 +20,21 @@ class Category(models.Model):
     # Поле category_description - описание категории продукта
     category_description = models.TextField(null=True, blank=True, unique=True, help_text="Описание категории")
 
+    def __str__(self):
+        return self.category_name
+
 
 # Модель Category - Категории продуктов
 class Product(models.Model):
     # Поле product_name - наименование продукта
     product_name = models.CharField(max_length=50, null=False, blank=False, unique=True, help_text="Продукт")
     # Поле product_category - категория продукта (ссылка на класс Category)
-    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     # Поле product_measure - единица измерения продукта (ссылка на класс Measure)
-    product_measure = models.ForeignKey(Measure, on_delete=models.CASCADE)
+    product_measure = models.ForeignKey(Measure, on_delete=models.SET_NULL, null=True)
     # Поле product_value - остаток продукта
     product_value = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
 
