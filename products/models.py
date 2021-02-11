df6e26a45be86a3d68fa4cf405d170be79532381
Alena <alynka2991@mail.ru>
2021-02-11 11:52:48 +0300

    Add a Measure model in products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Модель Measure - единицы измерения продуктов
+class Measure(models.Model):
+    # Поле measure_name - наименование единицы измерения
+    measure_name = models.CharField(max_length=50, null=False, blank=False, unique=True, help_text="Единица измерения")
+    # Поле short_measure_name - короткое наименование поля
+    short_measure_name = models.CharField(max_length=10, null=True, blank=True, unique=True, default=measure_name,
+                                          help_text="Короткое наименование единицы измерения")
