ea34c9a6a25a91be850555c496797ffd8d376ab4
Alena <alynka2991@mail.ru>
2021-02-11 12:45:39 +0300

    Add a Category model in products

--- products/models.py
+++ products/models.py
@@ -8,3 +8,11 @@ class Measure(models.Model):
     # Поле short_measure_name - короткое наименование поля
     short_measure_name = models.CharField(max_length=10, null=True, blank=True, unique=True, default=measure_name,
                                           help_text="Короткое наименование единицы измерения")
+
+
+# Модель Category - Категории продуктов
+class Category(models.Model):
+    # Поле category_name - наименование категории продукта
+    category_name = models.CharField(max_length=50, null=False, blank=False, unique=True, help_text="Категория")
+    # Поле category_description - описание категории продукта
+    category_description = models.TextField(null=True, blank=True, unique=True, help_text="Описание категории")
